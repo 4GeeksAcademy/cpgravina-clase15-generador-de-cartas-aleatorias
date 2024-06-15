2631e53d7afd521b5d8c1671ef85a8cd8ccf46eb
cpgravina <74896127+cpgravina@users.noreply.github.com>
2024-06-15 15:04:49 +0000

    Adding card generator feature

--- src/app.js
+++ src/app.js
@@ -7,5 +7,36 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const cardSymbols = ["♦", "♥", "♠", "♣"];
+  const cardNumbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, "J", "Q", "A"];
+
+  const RandomSymbolDisplayed =
+    cardSymbols[Math.floor(Math.random() * cardSymbols.length)];
+  const RandomNumberDisplayed =
+    cardNumbers[Math.floor(Math.random() * cardNumbers.length)];
+
+  document.getElementById("symbol").innerHTML = RandomSymbolDisplayed;
+  document.getElementById("number").innerHTML = RandomNumberDisplayed;
+  document.getElementById("symbol2").innerHTML = RandomSymbolDisplayed;
+
+  const symbolClass = document.getElementById("symbol");
+
+  if (
+    cardSymbols[2] == RandomSymbolDisplayed ||
+    cardSymbols[3] == RandomSymbolDisplayed
+  ) {
+    symbolClass.classList.remove("text-danger");
+    symbol.classList.add("text-black");
+  }
+
+  const symbol2Class = document.getElementById("symbol2");
+
+  if (
+    cardSymbols[2] == RandomSymbolDisplayed ||
+    cardSymbols[3] == RandomSymbolDisplayed
+  ) {
+    symbol2Class.classList.remove("text-danger");
+    symbol2.classList.add("text-black");
+  }
 };
